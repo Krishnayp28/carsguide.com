87e1d2d19fc22fd7ae06be2012dba3ba6231d437
jignaypatel <jignayp@yahoo.co.uk>
2023-05-19 19:35:52 +0100

    carsguide.com

---
+++ target/cucumber-report/cucumber.html/report.js
@@ -0,0 +1,489 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Buy.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Search functionality",
+  "description": "As a user I want to check search functionality of website.",
+  "id": "search-functionality",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 22,
+  "name": "Search the used car with model",
+  "description": "",
+  "id": "search-functionality;search-the-used-car-with-model",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 21,
+      "name": "@smoke"
+    },
+    {
+      "line": 21,
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 23,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 24,
+  "name": "I mouse hover on “buy+sell” tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 25,
+  "name": "I click ‘Used’ link",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "I navigate to ‘Used Cars For Sale’ page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 27,
+  "name": "I select make \"\u003cmake\u003e\" on used card search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "I select model \"\u003cmodel\u003e\" on used card search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "I select location \"\u003clocation\u003e\" on used card search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "I select price \"\u003cprice\u003e\" on used card search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "I click on Find My Next Car tab on used card search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "I should see the make \"\u003cmake\u003e\" in results",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 33,
+  "name": "",
+  "description": "",
+  "id": "search-functionality;search-the-used-car-with-model;",
+  "rows": [
+    {
+      "cells": [
+        "make",
+        "model",
+        "location",
+        "price"
+      ],
+      "line": 34,
+      "id": "search-functionality;search-the-used-car-with-model;;1"
+    },
+    {
+      "cells": [
+        "Audi",
+        "A3",
+        "NSW - All",
+        "$30,000"
+      ],
+      "line": 35,
+      "id": "search-functionality;search-the-used-car-with-model;;2"
+    },
+    {
+      "cells": [
+        "Kia",
+        "Rio",
+        "VIC - All",
+        "$30,000"
+      ],
+      "line": 36,
+      "id": "search-functionality;search-the-used-car-with-model;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 183193916800,
+  "error_message": "org.openqa.selenium.TimeoutException: java.util.concurrent.TimeoutException\nBuild info: version: \u00274.9.0\u0027, revision: \u0027d7057100a6\u0027\nSystem info: os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_121\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [438634047ca83acd295697005b33ae7d, get {url\u003dhttps://www.carsguide.com.au/}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 113.0.5672.127, chrome: {chromedriverVersion: 113.0.5672.63 (0e1a4471d5ae..., userDataDir: C:\\Users\\jigna\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:54476}, networkConnectionEnabled: false, pageLoadStrategy: normal, platformName: WINDOWS, proxy: Proxy(), se:cdp: ws://localhost:54476/devtoo..., se:cdpVersion: 113.0.5672.127, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:extension:minPinLength: true, webauthn:extension:prf: true, webauthn:virtualAuthenticators: true}\nSession ID: 438634047ca83acd295697005b33ae7d\r\n\tat org.openqa.selenium.remote.http.netty.NettyHttpHandler.makeCall(NettyHttpHandler.java:65)\r\n\tat org.openqa.selenium.remote.http.AddSeleniumUserAgent.lambda$apply$0(AddSeleniumUserAgent.java:42)\r\n\tat org.openqa.selenium.remote.http.Filter.lambda$andFinally$1(Filter.java:56)\r\n\tat org.openqa.selenium.remote.http.netty.NettyHttpHandler.execute(NettyHttpHandler.java:49)\r\n\tat org.openqa.selenium.remote.http.AddSeleniumUserAgent.lambda$apply$0(AddSeleniumUserAgent.java:42)\r\n\tat org.openqa.selenium.remote.http.Filter.lambda$andFinally$1(Filter.java:56)\r\n\tat org.openqa.selenium.remote.http.netty.NettyClient.execute(NettyClient.java:99)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:190)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:183)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:543)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.get(RemoteWebDriver.java:297)\r\n\tat com.carsguide.browserfactory.ManageBrowser.selectBrowser(ManageBrowser.java:36)\r\n\tat com.carsguide.Hooks.steUp(Hooks.java:16)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:40)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:34)\r\n\tat cucumber.runtime.java.JavaHookDefinition.execute(JavaHookDefinition.java:60)\r\n\tat cucumber.runtime.Runtime.runHookIfTagsMatch(Runtime.java:224)\r\n\tat cucumber.runtime.Runtime.runHooks(Runtime.java:212)\r\n\tat cucumber.runtime.Runtime.runBeforeHooks(Runtime.java:202)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:40)\r\n\tat cucumber.runtime.junit.ExecutionUnitRunner.run(ExecutionUnitRunner.java:102)\r\n\tat org.junit.runners.Suite.runChild(Suite.java:127)\r\n\tat org.junit.runners.Suite.runChild(Suite.java:26)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.runtime.junit.ExamplesRunner.run(ExamplesRunner.java:59)\r\n\tat org.junit.runners.Suite.runChild(Suite.java:127)\r\n\tat org.junit.runners.Suite.runChild(Suite.java:26)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.runtime.junit.ScenarioOutlineRunner.run(ScenarioOutlineRunner.java:53)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:63)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:18)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:70)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:95)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:38)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)\r\n\tat org.junit.internal.runners.statements.RunAfters.evaluate(RunAfters.java:27)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:309)\r\n\tat cucumber.api.junit.Cucumber.run(Cucumber.java:100)\r\n\tat org.junit.runner.JUnitCore.run(JUnitCore.java:160)\r\n\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:69)\r\n\tat com.intellij.rt.junit.IdeaTestRunner$Repeater$1.execute(IdeaTestRunner.java:38)\r\n\tat com.intellij.rt.execution.junit.TestsRepeater.repeat(TestsRepeater.java:11)\r\n\tat com.intellij.rt.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:35)\r\n\tat com.intellij.rt.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:235)\r\n\tat com.intellij.rt.junit.JUnitStarter.main(JUnitStarter.java:54)\r\nCaused by: java.util.concurrent.TimeoutException\r\n\tat java.util.concurrent.CompletableFuture.timedGet(CompletableFuture.java:1771)\r\n\tat java.util.concurrent.CompletableFuture.get(CompletableFuture.java:1915)\r\n\tat org.asynchttpclient.netty.NettyResponseFuture.get(NettyResponseFuture.java:206)\r\n\tat org.openqa.selenium.remote.http.netty.NettyHttpHandler.makeCall(NettyHttpHandler.java:59)\r\n\t... 70 more\r\n",
+  "status": "failed"
+});
+formatter.scenario({
+  "line": 35,
+  "name": "Search the used car with model",
+  "description": "",
+  "id": "search-functionality;search-the-used-car-with-model;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 21,
+      "name": "@smoke"
+    },
+    {
+      "line": 21,
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 23,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 24,
+  "name": "I mouse hover on “buy+sell” tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 25,
+  "name": "I click ‘Used’ link",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "I navigate to ‘Used Cars For Sale’ page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 27,
+  "name": "I select make \"Audi\" on used card search",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "I select model \"A3\" on used card search",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "I select location \"NSW - All\" on used card search",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "I select price \"$30,000\" on used card search",
+  "matchedColumns": [
+    3
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "I click on Find My Next Car tab on used card search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "I should see the make \"Audi\" in results",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "BuySteps.iAmOnHomepage()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "BuySteps.iMouseHoverOnBuySellTab()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "BuySteps.iClickUsedLink()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "BuySteps.iNavigateToUsedCarsForSalePage()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Audi",
+      "offset": 15
+    }
+  ],
+  "location": "BuySteps.iSelectMakeOnUsedCardSearch(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "A3",
+      "offset": 16
+    }
+  ],
+  "location": "BuySteps.iSelectModelOnUsedCardSearch(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "NSW - All",
+      "offset": 19
+    }
+  ],
+  "location": "BuySteps.iSelectLocationOnUsedCardSearch(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "$30,000",
+      "offset": 16
+    }
+  ],
+  "location": "BuySteps.iSelectPriceOnUsedCardSearch(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "BuySteps.iClickOnFindMyNextCarTabOnUsedCardSearch()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Audi",
+      "offset": 23
+    }
+  ],
+  "location": "BuySteps.iShouldSeeTheMakeInResults(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 123472978000,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 4249368500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 36,
+  "name": "Search the used car with model",
+  "description": "",
+  "id": "search-functionality;search-the-used-car-with-model;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 21,
+      "name": "@smoke"
+    },
+    {
+      "line": 21,
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 23,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 24,
+  "name": "I mouse hover on “buy+sell” tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 25,
+  "name": "I click ‘Used’ link",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "I navigate to ‘Used Cars For Sale’ page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 27,
+  "name": "I select make \"Kia\" on used card search",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "I select model \"Rio\" on used card search",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "I select location \"VIC - All\" on used card search",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "I select price \"$30,000\" on used card search",
+  "matchedColumns": [
+    3
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "I click on Find My Next Car tab on used card search",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "I should see the make \"Kia\" in results",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "BuySteps.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 193573600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BuySteps.iMouseHoverOnBuySellTab()"
+});
+formatter.result({
+  "duration": 1241451900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BuySteps.iClickUsedLink()"
+});
+formatter.result({
+  "duration": 1629426500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BuySteps.iNavigateToUsedCarsForSalePage()"
+});
+formatter.result({
+  "duration": 140209900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Kia",
+      "offset": 15
+    }
+  ],
+  "location": "BuySteps.iSelectMakeOnUsedCardSearch(String)"
+});
+formatter.result({
+  "duration": 245009000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Rio",
+      "offset": 16
+    }
+  ],
+  "location": "BuySteps.iSelectModelOnUsedCardSearch(String)"
+});
+formatter.result({
+  "duration": 172035000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "VIC - All",
+      "offset": 19
+    }
+  ],
+  "location": "BuySteps.iSelectLocationOnUsedCardSearch(String)"
+});
+formatter.result({
+  "duration": 155369500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "$30,000",
+      "offset": 16
+    }
+  ],
+  "location": "BuySteps.iSelectPriceOnUsedCardSearch(String)"
+});
+formatter.result({
+  "duration": 192763000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "BuySteps.iClickOnFindMyNextCarTabOnUsedCardSearch()"
+});
+formatter.result({
+  "duration": 2404996800,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Kia",
+      "offset": 23
+    }
+  ],
+  "location": "BuySteps.iShouldSeeTheMakeInResults(String)"
+});
+formatter.result({
+  "duration": 429455900,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 849600200,
+  "status": "passed"
+});
+});
